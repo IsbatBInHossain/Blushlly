4852ad64b26486a6c46b740f92acc2918f7e6ea2
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-10-18 00:39:03 +0600

    Add tailwind configurations for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'hsl(0 5% 11%)',
+          light: 'hsl(0 5% 11% / 0.6)',
+        },
+        'dark-accent': 'hsl(0, 0%, 100% /0.1)',
+        grey: {
+          lead: '#6F6F6F',
+          iron: '#838383',
+          light: '#DDDDDD',
+          silver: '#CFCFCF',
+          ash: '#535353',
+          drab: '#9B9B9B',
+        },
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        viadaloka: ['Vidaloka', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
